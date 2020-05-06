72451c77d47787ac4b991ba66702a22be7e5feaa
mikifriki <mihkel.liblikman@gmail.com>
2020-05-06 21:42:42 +0300

    Added heroku port support

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.get('/api/cards/:id', (req, res) => {
 	res.json(card);
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
 	`Server running on port ${port}`
 });
